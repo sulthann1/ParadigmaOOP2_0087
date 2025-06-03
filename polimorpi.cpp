73a66a1248ea2da335bb36271c74b3b56d1b4df8
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 14:30:20 +0700

    menambahkan class joko

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 class seseorang{
-    //virtual void pesan 
+    //virtual void pesan ()=0
+    virtual void pesan (){
+        cout << "Pesan dari seseorang" <<endl;
+    }
 };
+
+class joko : public
