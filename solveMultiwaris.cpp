c6dab90293f119a4163a3685588f031f472d16a0
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 15:18:22 +0700

    menambahkan class pekerja

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -12,3 +12,12 @@ class orang {
     }
 };
 
+class pekerja : public orang{
+    public :
+    pekerja( int pUmur) :
+    orang (pUmur)
+    {
+        cout << "pekerja dibuat \n" <<endl;
+    }
+};
+
