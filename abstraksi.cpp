83d4ac7d98909c87203d0dec6fb40db9e839d493
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 14:44:42 +0700

    menambahkan void display untuk menampilkkan nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -8,5 +8,13 @@ class AbstraksiKlas{
 
     //method untuk mengisi nilai
     //private member 
-    
-}
+    void setXY (string a, string b){
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display (){
+        cout << "x = " << x <<endl;
+        cout << "y = " << y <<endl;
+    }
+};
