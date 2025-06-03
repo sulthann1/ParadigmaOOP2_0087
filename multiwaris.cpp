99d89206c9193f8611f722aff0aa887dcbd03b04
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 15:09:02 +0700

    menambabhkan class pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,3 +11,11 @@ class orang {
         cout << "orang dibuat dengan umur" << umur << "\n" <<endl;
     }
 };
+class pelajar : virtual public orang {
+    public :
+    pelajar (int pUmur) :
+    orang (pUmur) 
+    {
+        cout << "pelajar dibuat dengan umur\n" <<endl; 
+    }
+};
