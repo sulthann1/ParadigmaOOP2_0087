e25930a778e8fdcdfca6a369496cab9c4083ee18
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 15:02:51 +0700

    menambahkan library

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -4,7 +4,29 @@ using namespace std;
 class remoteLampu{
     private :
     string saklarNo [10];
-    pubilc :
+    public :
+    void setSaklarNo (int i, string value ){
+        saklarNo [i] = value ;
+    }
+
+    string setSaklarNo (int i){
+        return saklarNo[i];
+    }
+};
+
+int main(){
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar  Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
     
-}
+    cout << lampuRumah.setSaklarNo[0] << endl;
+    cout << lampuRumah.setSaklarNo[1] << endl;
+    cout << lampuRumah.setSaklarNo[2] << endl;
+    cout << lampuRumah.setSaklarNo[3] << endl;
+     return 0;
+
+};
 
