e1715fea600561800246c3d6f00f9029943b2163
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 15:22:38 +0700

    menambahkan class budi pekerja pelajar

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -31,5 +31,20 @@ orang (pUmur)
 }
 };
 
-class budi
+class budi : public pekerja, public pelajar{
+    public  :
+    budi (int pUmur) :
+    pekerja (pUmur),
+    pelajar (pUmur)
+
+    {
+        cout << "budi dibuat \n" <<endl;
+    }
+};
+
+int main () {
+    budi a(12);
+
+    return 0;
+}
 
