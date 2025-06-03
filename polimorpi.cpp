cc3ac05fa5c79131625362c0efcfdb03e33f81b7
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-03 14:39:45 +0700

    return poli

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class seseorang{
+    public :
     //virtual void pesan ()=0
     virtual void pesan (){
         cout << "Pesan dari seseorang" <<endl;
@@ -10,14 +11,28 @@ class seseorang{
 
 class joko : public seseorang {
     public :
-    void pesan (){
+    void pesan () {
         cout << "Pesan dari joko" <<endl;
     }
 };
 
 class lia : public seseorang {
     public :
-    void pesan (){
+    void pesan () {
         cout << "pesan dari lia" <<endl;
     }
+};
+
+int main (){
+    seseorang *obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek ->pesan();
+    obyek = &b;
+    obyek ->pesan();
+    // a.seseorang::pesan
+    //b. seseorang::pesan
+    return 0;
 };
